cd00e1ebe9283301dc7bd590dc428f25a659894f
Eric <elazarski94@gmail.com>
2019-05-06 18:14:28 -0500

    gensim model with corrected bigrams

--- cook_bot/generate_gensim.py
+++ cook_bot/generate_gensim.py
@@ -2,16 +2,13 @@
 import gensim
 import csv
 recipes = []
-with open('myrecipes.csv', mode='r') as infile:
-    reader = csv.reader(infile):
-with open('myrecipes.csv', mode='r') as infile:
+with open('bigram_corrected_recipes.csv', mode='r') as infile:
     reader = csv.reader(infile)
     for row in reader:
         recipes.append(row)
         
 recipes[0]
 model = gensim.models.Word2Vec(recipes)
-model.wb.similarity('EGG', 'CHICKEN')
 model.wv.similarity('EGG', 'CHICKEN')
 model.wv.most_similar(positive=['EGG'], topn=3)
 import pickle
